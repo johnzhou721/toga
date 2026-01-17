fe767af61ccc6179ed319224f6ae092202c85125
John <john-xyz@outlook.com>
2026-01-16 19:06:24 -0600

    Refactor redraw call for better readability

--- testbed/tests/conftest.py
+++ testbed/tests/conftest.py
@@ -134,7 +134,10 @@ async def window_cleanup(app, app_probe, main_window, main_window_probe):
     await main_window_probe.wait_for_window(
         "Resetting main_window", state=WindowState.NORMAL
     )
-    await probe.redraw("Waiting for current window to focus", wait_for=lambda: (app.current_window == main_window))
+    await probe.redraw(
+        "Waiting for current window to focus",
+        wait_for=lambda: (app.current_window == main_window),
+    )
 
     yield
 
